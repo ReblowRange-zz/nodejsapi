e97566dcb3904beb7d352a9bc76f23999d5743fa
ReblowRange <reblowrange@gmail.com>
2022-01-02 00:31:01 +0530

    `

--- src/controller.js
+++ src/controller.js
@@ -4,9 +4,11 @@ import {
   Json,
   KoaController,
   Post,
+  Put
 } from "koa-joi-controllers";
 
 import { Report } from "./models";
+import * as RestService from "./service";
 
 @Controller("/v1")
 export class RestController extends KoaController {
@@ -15,13 +17,30 @@ export class RestController extends KoaController {
     ctx.body = "Test rest";
   }
 
-  @Post("/createReport")
+  @Put("/report")
   @Json()
   async createExportReport(ctx) {
+    const body = ctx.request.body; 
+    // const report = Report.fromJson(body);
+    ctx.body = await RestService.createReport(body);
+  }
+
+  @Post("/report")
+  @Json()
+  async updateExportReport(ctx) {
     const body = ctx.request.body; // incoming JSON data is in ctx.request.body
-    const report = Report.fromJson(body);
-    console.log(report.name);
-    ctx.body = report;
-    // process.stdout.write(`${JSON.stringify(body)}`);
+    // const report = Report.fromJson(body);
+    ctx.body = await RestService.updateReport(body);
+  }
+
+  @Get("/getReport/:id")
+  async getReport(ctx) {
+    try {
+      const reportId = ctx.params.id;
+      console.log(reportId);
+      ctx.body = await RestService.getReport(reportId);
+    } catch (err) {
+      ctx.throw(500, err.message);
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Koa from "koa";
 import { configureRoutes } from "koa-joi-controllers";
 import { RestController } from "./controller";
-import {stdout} from 'single-line-log2';
+import { stdout } from "single-line-log2";
 
 /* ************** Import End *********************/
 const appServer = new Koa();
@@ -10,23 +10,17 @@ stdout(`=============== Starting server ===============`);
 const PORT = process.env.PORT || 3000;
 configureRoutes(appServer, [new RestController()]);
 
-/* appServer.use(async (ctx) => {
-  // await Promise;
-  console.log(ctx.req.method);
-  // ctx.throw(401, 'name required');
-  ctx.requ;
-  ctx.body = "Hello World";
+/* For every error */
+/* appServer.on("error", (err) => {
+  console.error("server error", err);
 }); */
 
-/* For every error */
-appServer.on("error", (err) => {
-  console.error("server error", err);
-});
 /* If an error is in the req/res cycle and it is not possible to respond to the client,
  * the Context instance is also passed
  */
 appServer.on("error", (err, ctx) => {
-  console.error("server error", err, ctx);
+  console.log("---------------------server error", err, ctx);
+  // ctx.throw(401, err.message);
 });
 
 appServer.listen(PORT, () => {
